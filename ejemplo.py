4c8671ebc7e5bca8d8f8236c632c0f38c929b759
Alejandro <al.sepulvedam@profesor.duoc.cl>
2022-03-09 16:01:34 -0300

    Corregidos los inputs
    Ya no esta feo

--- ejemplo.py
+++ ejemplo.py
@@ -1,7 +1,7 @@
 #Acá empieza el proyecto
 x=0
 y=0
-x=int(input("Ingrese dato x"))
-y=int(input("Ingrese y"))
+x=int(input("Ingrese dato x: "))
+y=int(input("Ingrese y: "))
 res=x+y
 print(f"El resultado es {res}")
